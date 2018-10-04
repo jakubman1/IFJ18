ac5baa6cdf54f486ec3f8d5da90db0d27e1f9330
Jakub Man <jakubman1@gmail.com>
2018-10-04 11:32:47 +0200

    Add basic project files

---
+++ types.h
@@ -0,0 +1,9 @@
+/**
+ * @file types.h
+ * @author Jakub Man
+ * @author Jiří Tykva
+ * @date 4.10. 2018
+ * @brief Definition of types required for the project
+ */
+
+// TODO: Define required types
