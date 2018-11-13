7e51f67463ba8016ae264b9443e93ca706acc5bd
Jakub Man <jakubman1@gmail.com>
2018-11-13 18:29:44 +0100

    TESTS: Small visual changes

--- test/runtest.sh
+++ test/runtest.sh
@@ -11,14 +11,13 @@ NC='\033[0m' # No Color
 ########
 echo -e "\n\n"
 echo "Running tests for lexical analysis"
-echo -e "\n"
-# List of all files
-for FILENAME in 'nocomment' 'factorial' 'comments'
+echo "----------------------------------"
+for FILENAME in 'nocomment' 'factorial' 'comments' # List of all test files
 do
   ./lexical-test < test/$FILENAME.rb > $FILENAME.lexical.tmp
   DIFF=$(diff $FILENAME.lexical.tmp test/$FILENAME.lexical.output)
   if [ "$DIFF" != "" ] ; then
-    echo -e "${RED}ERROR, ${FILENAME}.rb lexical analysis not passed!${NC}"
+    echo -e "${RED}ERROR, ${FILENAME}.rb: lexical analysis not passed!${NC}"
     echo $DIFF
   else
     echo -e "${GREEN}Lexical analysis of ${FILENAME}.rb passed!${NC}"
