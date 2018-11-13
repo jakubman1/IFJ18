0e53873134f2e677e69d038e74dbe1e71595a624
Jakub Man <jakubman1@gmail.com>
2018-11-13 17:34:03 +0100

    TEST: Add colors to make them more fun

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+########
+# Define colors
+########
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 ########
 # Test for lexical analysis
@@ -7,18 +13,18 @@
 ./lexical-test < test/factorial.rb > factorial.lexical.tmp
 DIFF=$(diff factorial.lexical.tmp test/factorial.lexical.output)
 if [ "$DIFF" != "" ] ; then
-  echo "ERROR, factorial.rb lexical analysis not passed!"
+  echo "${RED}ERROR, factorial.rb lexical analysis not passed!${NC}"
   echo $DIFF
 else
-  echo "Lexical analysis of factorial.rb passed"
+  echo "${GREEN}Lexical analysis of factorial.rb passed${NC}"
 fi
 
 #comments.rb
 ./lexical-test < test/comments.rb > comments.lexical.tmp
 DIFF=$(diff comments.lexical.tmp test/comments.lexical.output)
 if [ "$DIFF" != "" ] ; then
-  echo "ERROR, comments.rb lexical analysis not passed!"
+  echo "${RED}ERROR, comments.rb lexical analysis not passed!${NC}"
   echo $DIFF
 else
-  echo "Lexical analysis of factorial.rb passed"
+  echo "${GREEN}Lexical analysis of factorial.rb passed${NC}"
 fi
