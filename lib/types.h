242ba7a61646a4d10182e118936ade3fd5b79595
Jakub Man <jakubman1@gmail.com>
2018-11-22 18:16:01 +0100

    Add ll table enum to types lib

--- lib/types.h
+++ lib/types.h
@@ -33,8 +33,46 @@ typedef enum {
   OPERATOR, // Operator, eg +, -, /, (, )... 15
   STRING, //16
   ERROR, // Error type (unknown character) 17
-  EOL // 18
+  EOL, // 18
+  COMMA
 } token_type;
+
+typedef enum {
+  LL_PROG = 100,
+  LL_PARAMS,
+  LL_PARAMS_N,
+  LL_STATEMENT,
+  LL_DEF_ARGS,
+  LL_DEFINE,
+  LL_ARGS,
+  LL_ARGS_N,
+  LL_VALUE,
+  LL_EXPRESSION,
+  LL_DEF = 1000,
+  LL_ID,
+  LL_EOL,
+  LL_END,
+  LL_COMA,
+  LL_IF,
+  LL_THEN,
+  LL_ELSE,
+  LL_WHILE,
+  LL_DO,
+  LL_EQUAL,
+  LL_PRINT,
+  LL_INPUTS,
+  LL_INPUTI,
+  LL_INPUTF,
+  LL_LENGHT,
+  LL_SUBSTR,
+  LL_ORD,
+  LL_CHR,
+  LL_BRACKET_LEFT,
+  LL_BRACKET_RIGHT
+  LL_INT,
+  LL_FLOAT,
+  LL_STRING
+} ll_table_type;
 
 typedef struct token {
   char *text;
