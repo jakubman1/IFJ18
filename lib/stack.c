0cdcf5de33f354f2800b31e5c0499dc3b4fabed9
Jakub Man <jakubman1@gmail.com>
2018-11-22 17:57:37 +0100

    Add stack library

---
+++ lib/stack.c
@@ -0,0 +1,49 @@
+void s_init(tStack *stack)
+{
+  stack->head = NULL;
+}
+
+void s_push(tStack *stack, int data)
+{
+  tStackNode *new = malloc(sizeof(tStackNode));
+  if(new != NULL) {
+    new->data = data;
+    new->next = stack->head;
+    stack->head = new;
+  }
+  else {
+    fprintf(stderr, "Internal Error: No memory left.\n");
+    exit(99);
+  }
+
+}
+
+int s_pop(tStack *stack)
+{
+  if(!s_empty(stack)) {
+    int data = stack->head->data;
+    tStackNode *tmp = stack->head;
+    stack->head = stack->head->next;
+    free(tmp);
+    return data;
+  }
+  else {
+    return -1;
+  }
+}
+
+int s_top(tStack *stack)
+{
+  if(!s_empty(stack)) {
+    return stack->head->data;
+  }
+  else {
+    return -1;
+  }
+
+}
+
+bool s_empty(tStack *stack)
+{
+  return stack->head == NULL;
+}
---
+++ lib/stack.h
@@ -0,0 +1,28 @@
+/**
+ * @file stack.h
+ * @author Jakub Man
+ * @date 22.11. 2018
+ * @brief Stack library
+ */
+
+#ifndef STACK_H
+#define STACK_H
+#include <stdbool.h>
+#include "types.h"
+
+typedef struct stackNode {
+  int data;
+  struct stackNode *next;
+} tStackNode;
+
+typedef struct stack {
+  tStackNode *head;
+} tStack;
+
+void s_init(tStack *stack);
+void s_push(tStack *stack, int data);
+int s_pop(tStack *stack);
+int s_top(tStack *stack);
+bool s_empty(tStack *stack);
+
+#endif
