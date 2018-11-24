fb4e7cc8c33c6d42403a790ac6af0e914016cdf7
Jakub Man <jakubman1@gmail.com>
2018-11-24 17:39:15 +0100

    Add basic files for print-code funcitons

---
+++ print-code.h
@@ -0,0 +1,12 @@
+/**
+ * @file print-code.h
+ * @author Jakub Man
+ * @date 24.11. 2018
+ * @brief Functions for printing parts of code.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printFloat(float val);
+void printInt(int val);
