b0e518171807aac0c5f4d6053934063f26e4779a
Jakub Man <jakubman1@gmail.com>
2018-11-24 16:13:50 +0100

    Update symtable types to new format

--- symtable.h
+++ symtable.h
@@ -1,106 +1,73 @@
+/**
+ * @file symtable.h
+ * @author Jakub Man
+ * @date 24.11. 2018
+ * @brief Data types and functions for symbol table.
+ */
+
 #ifndef SYMTABLE_H
 #define SYMTABLE_H
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-/**
- * @brief Struct of a data in a binary tree node.
- *
- * #TODO - search for correct data for Symbol Table.
- *              name
- *              type1 (int, double, ...)
- *              type2 (constant, variable, procedure)
- *              number and types of arguments in procedures
- *
- * @param i asd.
- * @param c asd.
- */
-typedef struct data {
-    int i;
-    char c;
-} Tdata;
+#include "lib/errcodes.h"
 
-/**
- * @brief Struct of a binary tree node.
- *
- * @param left Pointer to the left child node.
- * @param right Pointer to the right child node.
- * @param data Data which the node contains.
- */
-typedef struct node {
-    struct node *left;
-    struct node *right;
-    Tdata data;
-} Tnode;
 
-/**
- * #TODO - orders alphabetically?
- *
- * Alocates memory for new binary tree node. Inserts data into the node and sets it's child nodes to NULL.
- *
- * @brief Creates new binary tree node.
- *
- * @param data Pointer to Tdata which are inserted into the node.
- * @return Pointer to the new node.
- */
-Tnode *createNewNode (Tdata data);
+typedef enum {
+  VARIABLE,
+  FUNCTION
+} symbol_type;
+typedef enum {
+  TYPE_INT,
+  TYPE_STRING,
+  TYPE_FLOAT,
+  TYPE_NIL
+} data_type;
 
-/**
- * The function creates a new node and places it into the binary tree so it's ordered #TODO alphabetically?
- *
- *@brief Inserts a node in the binary tree.
- *
- * @param root Pointer to the binary tree root where the function inserts node.
- * @param data Data which function inserts into the node.
- * @return Pointer to the root node.
- */
-Tnode *insertNode (Tnode *root, Tdata data);
+/* typedef union {
+  int ival;
+  double fval;
+  char *sval;
+} tVarVal;*/
 
-/**
- * @brief Searches for the match of data in binary tree.
- *
- * @param root Pointer to the binary tree root where the function searches for the match.
- * @param data Data which the function searches for.
- * @return True if it founds the match, False if not.
- */
-bool isInBinaryTree (Tnode *root, Tdata data);
+typedef struct funcParam {
+  data_type type;
+  struct funcParam *next;
+} tFuncParam;
 
-/**
- * Function compares two integers and selects the greater one. If the integers are equal, the function chooses the first one.
- * Auxiliary function for binaryTreeHeight
- *
- * @brief Selects the greater integer.
- *
- * @param a First number to compare
- * @param b Second number to compare
- * @return The greater number.
- */
-int maximum (int a, int b);
+typedef struct variableData {
+  // tVarVal value;
+  bool defined;
+  data_type type;
+} tVariableData;
 
-/**
- *@brief Determines the maximum height of a binary tree.
- *
- * @param root Pointer to the root of the binary tree.
- * @return Maximum height of the binary tree.
- */
-int binaryTreeHeight (Tnode *root);
+typedef struct functionData {
+  bool defined;
+  data_type returnType;
+  int paramCount;
+  tFuncParam *params;
+} tFuncitonData;
 
-/**
- * The function creates new binary tree and copies the structure and data of the original binary tree in the new one.
- *
- * @brief Copies binary tree.
- *
- * @param origRoot Pointer to the root of the binary tree that is going to be copied.
- * @return Pointer to the root of the new copied binary tree.
- */
-Tnode *copyBinaryTree (Tnode *origRoot);
+typedef union {
+  tVariableData varData;
+  tFuncitonData funData;
+} tDataType;
 
-/**
- * Deletes every node in the Binary Tree. The function traverses tree in post order. After the function, the root should be set to NULL.
- *
- *@brief Deletes the binary tree.
- *
- * @param root Pointer to the root of the Binary Tree which is going to be deleted.
- */
-void deleteBinaryTree (Tnode *root);
+typedef struct symNode {
+  char *name;
+  symbol_type type;
+  tDataType data; // Based on symbol type
+  struct symNode *left;
+  struct symNode *right;
+} *tSymPtr;
+
+void symtable_init(tSymPtr *root);
+void symtable_insert_variable(tSymPtr *root, char *name, data_type type);
+void symtable_insert_function(tSymPtr *root, char *name, data_type returnType, int paramCount, tFuncParam *params, bool define);
+void symtable_insert(tSymPtr *root, tSymPtr node);
+tSymPtr symtable_search(tSymPtr *root, char *name);
+void symtable_clear(tSymPtr *root);
+
 
 
 #endif
