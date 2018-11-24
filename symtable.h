b0851edfe63561af7a8ed41cc1691b79d7177174
Jakub Man <jakubman1@gmail.com>
2018-11-24 17:27:56 +0100

    Add docs to symtable

--- symtable.h
+++ symtable.h
@@ -62,11 +62,46 @@ typedef struct symNode {
   struct symNode *rptr;
 } *tSymPtr;
 
+/**
+Initialize symtable
+*/
 void symtable_init(tSymPtr *root);
+/**
+Insert a new variable into symtable. If variable already exists, update it.
+@param root Root of symtable tree.
+@param name Name of a variable
+@param type Type of variable (can be TYPE_INT, TYPE_STRING, TYPE_FLOAT or TYPE_NIL)
+@param define Was variable defined or not? If yes, variable will be set as defined
+@returns 0 on success, 2 if name is a function, 99 on memory allocation errors.
+*/
 int symtable_insert_variable(tSymPtr *root, char *name, data_type type, bool define);
+/**
+Insert a new function into symtable. If function already exists, update it.
+@param root Root of symtable tree.
+@param name Name of a function
+@param returnType Return type of the function (can be TYPE_INT, TYPE_STRING, TYPE_FLOAT or TYPE_NIL)
+@param paramCount Number of parameters that the function has.
+@param params Single-linked list of function parameters
+@param define Was function defined (true) or just called (false)?
+@returns 0 on success, 2 if name is a variable, 99 on memory allocation errors.
+*/
 int symtable_insert_function(tSymPtr *root, char *name, data_type returnType, int paramCount, tFuncParam *params, bool define);
+/**
+Helper function for inserting into symtable.
+Do not call this function directly !
+*/
 void symtable_insert(tSymPtr *root, tSymPtr node);
+/**
+Search in symtable by name
+@param root Root of symtable tree
+@param name Name of the symbol you want to find
+@returns pointer to symbol or NULL if nothing was found
+*/
 tSymPtr symtable_search(tSymPtr root, char *name);
+/**
+Free all allocated resources of a symtable, including parameter lists.
+@param root Root of a symtable
+*/
 void symtable_clear(tSymPtr *root);
 
 
