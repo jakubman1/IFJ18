e7129fc3fccab8f1cacf1e130db9418e4a978db8
Jakub Man <jakubman1@gmail.com>
2018-11-24 17:39:27 +0100

    Implement expression parsing

--- expression.c
+++ expression.c
@@ -1,8 +1,96 @@
 /**
  * @file expression.c
- * @author
+ * @author Jiri Tykva
+ * @author Jakub Man
+ * @author Adam Melichar
+ * @author Jan Martinák
  * @date 24.11. 2018
  * @brief Implementation of "expression"
  */
 
 #include "expression.h"
+
+/*
+*     // TODO
+*     POKUD V LL_PREDICT FUNKCI DOJDE then, do NEBO eol TAK MUSIME EXPRESSION ZAVOLAT JESTE JEDNOU S NEJAKYM
+*     SPECIALNIM SYMBOLEM KTERY BUDE ZNACIT, ZE JE KONEC EXPRESSIONU
+*
+*/
+
+int prec_table (tToken *token)
+{
+  int result = SUCCESS; // TODO
+  int prec_table_out = SUCCESS;
+  tToken currentToken = {"", ERROR};
+
+  static tStack stack_rules; // stores postfix notation
+  static tStack stack_pushdown; // auxiliary stack
+  static bool init = false;
+
+  if (!init)
+  {
+    s_init(&stack_rules);
+    s_init(&stack_pushdown);
+
+    s_push(&stack_pushdown, P_BOTTOM);
+
+    init = true;
+  }
+
+  int token_input;
+  if (token->type == OPERATOR && token->text[0] == '+') {
+    token_input = 0;
+  }
+  else if (token->type == OPERATOR && token->text[0] == '-') {
+    token_input = 1;
+    //TODO
+  }
+  else if (token->type == OPERATOR && token->text[0] == '*') {
+    token_input = 2;
+  }
+  else if (token->type == OPERATOR && token->text[0] == '/') {
+    token_input = 3;
+  }
+  else if (token->type == OPERATOR && token->text[0] == '<') {
+    token_input = 4;
+  }
+  else if (token->type == OPERATOR && (strcmp(token->text, "<=") == 0)) {
+    token_input = 5;
+  }
+  else if (token->type == OPERATOR && token->text[0] == '>') {
+    token_input = 6;
+  }
+  else if (token->type == OPERATOR && (strcmp(token->text, ">=") == 0)) {
+    token_input = 7;
+  }
+  else if (token->type == OPERATOR && (strcmp(token->text, "==") == 0)) {
+    token_input = 8;
+  }
+  else if (token->type == OPERATOR && (strcmp(token->text, "!=") == 0)) {
+    token_input = 9;
+  }
+  else if (token->type == OPERATOR && token->text[0] == '(') {
+    token_input = 10;
+  }
+  else if (token->type == OPERATOR && token->text[0] == ')') {
+    token_input = 11;
+  }
+  else if (token->type == ID || token->type == INTEGER || token->type == FLOATING_POINT || token->type == STRING) { // i
+    token_input = 12;
+  }
+  else { // end of expression (== $)
+    token_input = 13;
+  }
+
+
+
+  switch (precedent_table[s_top(&stack_pushdown)][token_input])
+  {
+    case E: // =
+      break;
+    case R: // >
+      break;
+    case S: // <
+      break;
+  }
+}
--- expression.h
+++ expression.h
@@ -7,7 +7,11 @@
  #ifndef EXPRESION_H
  #define EXPRESION_H
 
- #define TABLE_S 9
+ #include <stdbool.h>
+ #include <string.h>
+ #include "lib/stack.h"
+
+#define PRECEDENT_TABLE_SIZE 14
 
 typedef enum {
   E, // =
@@ -16,55 +20,62 @@ typedef enum {
   N // NONE
 } p_table_sign;
 
-static int precedent_table[TABLE_S][TABLE_S] =
+static int precedent_table[PRECEDENT_TABLE_SIZE][PRECEDENT_TABLE_SIZE] =
 {
-  //+-|*/| r| e| (| )| i| $|
-   {R, S, R, R, S, R, S, R}, // +-
-   {R, R, R, R, S, R, S, R}, // */
-   {S, S, N, R, S, R, S, R}, // r
-   {S, S, S, N, S, R, S, R}, // e
-   {S, S, S, S, S, E, S, N}, // (
-   {R, R, R, R, N, R, N, R}, // )
-   {R, R, R, R, N, R, N, R}, // i
-   {S, S, S, S, S, N, S, N}  // $
+  //+ |- |* |/ |< |<=|> |>=|==|!=|( |) |i |$ |
+   {R, R, S, S, R, R, R, R, R, R, S, R, S, R}, // +
+   {R, R, S, S, R, R, R, R, R, R, S, R, S, R}, // -
+   {R, R, R, R, R, R, R, R, R, R, S, R, S, R}, // *
+   {R, R, R, R, R, R, R, R, R, R, S, R, S, R}, // /
+   {S, S, S, S, N, N, N, N, R, R, S, R, S, R}, // <
+   {S, S, S, S, N, N, N, N, R, R, S, R, S, R}, // <=
+   {S, S, S, S, N, N, N, N, R, R, S, R, S, R}, // >
+   {S, S, S, S, N, N, N, N, R, R, S, R, S, R}, // >=
+   {S, S, S, S, S, S, S, S, N, N, S, R, S, R}, // ==
+   {S, S, S, S, S, S, S, S, N, N, S, R, S, R}, // !=
+   {S, S, S, S, S, S, S, S, S, S, S, E, S, N}, // (
+   {R, R, R, R, R, R, R, R, R, R, N, R, N, R}, // )
+   {R, R, R, R, R, R, R, R, R, R, N, R, N, R}, // i
+   {S, S, S, S, S, S, S, S, S, S, S, N, S, N}  // $
 };
 
-/*
-E → E + E
-E → E - E
-E → E * E
-E → E / E
-E → (E)
-E → i
-E → int
-E → float
-E → string
-E → E < E
-E → E <= E
-E → E > E
-E → E >= E
-E → E == E
-E → E != E
-*/
 
 typedef enum {
-  EXP,
-  E_PLUS_E,
-  E_MINUS_E,
-  E_MULTIPLY_E,
-  E_DIVIDE_E,
-  BRACKET_E_BRACKET,
-  ID,
-  INT,
-  FLOAT,
-  STRING,
-  E_LOWER_E,
-  E_LOWEREQ_E,
-  E_GREATER_E,
-  E_QREATEREQ_E,
-  E_EQ_E,
-  E_NOTEQ_E
-} expression_rules;
+  P_PLUS,           // 0
+  P_MINUS,          // 1
+  P_MULTIPLY,       // 2
+  P_DIVIDE,         // 3
+  P_LOWER,          // 4
+  P_LOWEREQ,        // 5
+  P_GREATER,        // 6
+  P_GREATEREQ,      // 7
+  P_EQ,             // 8
+  P_NOTEQ,          // 9
+  P_LEFT_BRACKET,   // 10
+  P_RIGHT_BRACKET,  // 11
+  P_ID,             // 12
+  P_BOTTOM          // 13
+} p_table_terms;
+
+/*
+typedef enum {
+  E_PLUS_E,             // E → E + E
+  E_MINUS_E,            // E → E - E
+  E_MULTIPLY_E,         // E → E * E
+  E_DIVIDE_E,           // E → E / E
+  BRACKET_E_BRACKET,    // E → (E)
+  ID,                   // E → i
+  INT,                  // E → int
+  FLOAT,                // E → float
+  STRING,               // E → string
+  E_LOWER_E,            // E → E < E
+  E_LOWEREQ_E,          // E → E <= E
+  E_GREATER_E,          // E → E > E
+  E_QREATEREQ_E,        // E → E >= E
+  E_EQ_E,               // E → E == E
+  E_NOTEQ_E             // E → E != E
+  EXP,                  // E
+} expression_rules;*/
 
 /* #define PREC_RULE_01 s_pop(stack); s_push(stack, ); s_push(stack, ); s_push(stack, ) */
 
