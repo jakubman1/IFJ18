a4f4b5741f1be13c3b65246237ecaf396dba3df7
ketchapp1 <33194928+ketchapp1@users.noreply.github.com>
2018-11-29 16:47:01 +0100

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -171,19 +171,8 @@ int scanner(tToken *token_out)
         }
         break;
       case STRING_ESCAPING:
-
-        if (c == '\') {
-          add_to_buffer(&buffer, &buff_size, c);
-          if ((c == getc(stdin)) == '"') {
-            add_to_buffer(&buffer, &buff_size, c);
-            state = STRING_START;
-            break;
-          } else {
-            ungetc(c, stdin);
-          }
-          state = STRING_START;
-        }
-        else if (c != '"' && c != '\n') {
+      
+        if (c != '"' && c != '\n') {
           add_to_buffer(&buffer, &buff_size, c);
           state = STRING_START;
         }
