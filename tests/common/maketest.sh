99bfbac9a73e064498fa7457c5a67af0552bcf48
Jakub Man <jakubman1@gmail.com>
2018-12-03 11:06:04 +0100

    Fix maketest.sh to get data from correct directory

--- tests/common/maketest.sh
+++ tests/common/maketest.sh
@@ -1,4 +1,4 @@
 for FILENAME in 'comments' 'factorial' 'factorial2' 'floats' 'lexerr' 'multiparam' 'strings' 'undef-var' # List of all test files
 do
-  ./lexical-test.exe < test/srcs/$FILENAME.rb > test/lexical/$FILENAME.lexical.output
+  ./lexical-test.exe < tests/srcs/$FILENAME.rb > tests/lexical/$FILENAME.lexical.output
 done
